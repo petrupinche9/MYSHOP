f311e43fff9913aa68ebaf10534803c6d364e073
AlessioS98 <99815094+AlessioS98@users.noreply.github.com>
2022-02-19 21:00:56 +0100

    Add files via upload

--- MYSHOP/src/Principale.java
+++ MYSHOP/src/Principale.java
@@ -5,8 +5,9 @@ public class Principale
 {
     public static void main(String[] args)
     {
+        guest utente = new guest();
         MENU interfaccia = new MENU();
-        boolean confronta1, confronta2, confronta3;
+        boolean confronta1, confronta2, confronta3, confronta4, confronta5;
         String scelta = JOptionPane.showInputDialog("Benvenuto nel nostro MyShop \n" +
                 "Sei Amministratore, Manager oppure Utente?");
         confronta1 = scelta.equals("Amministratore");
@@ -25,7 +26,37 @@ public class Principale
         }
         if (scelta.equals("Utente"))
         {
-            interfaccia.GUI();
+            String risposta = JOptionPane.showInputDialog("Salve \n" +
+                    "Grazie per aver scelto il nostro MyShop. Speriamo sia di tuo gradimento \n" +
+                    "Sei un utente registrato? (Rispondi con SI o NO)");
+            confronta4 = risposta.equals("SI");
+            confronta5 = risposta.equals("NO");
+            if (confronta4 == false || confronta5 == false)
+            {
+                do
+                {
+                    risposta = JOptionPane.showInputDialog("ATTENZIONE!!! \n" +
+                            "Inserimento non valido! \n" +
+                            "RIPROVARE");
+                    confronta4 = risposta.equals("SI");
+                    confronta5 = risposta.equals("NO");
+                } while (confronta4 == false && confronta5 == false)
+            }
+            if (risposta.equals("SI"))
+            {
+                utente.visitaShop();
+            } else
+            {
+                String risposta2 = JOptionPane.showInputDialog("Vuoi effettuare la registrazione? " +
+                        "(Rispondi con SI o NO)");
+                if (risposta2.equals("NO"))
+                {
+                    utente.visitaShop();
+                } else
+                {
+                    utente.registrazione();
+                }
+            }
         }
     }
 }
--- MYSHOP/src/guest.java
+++ MYSHOP/src/guest.java
@@ -1,2 +1,15 @@
-public class guest {
+import javax.swing.*;
+
+public class guest
+{
+    MENU grafica = new MENU();
+    public void visitaShop()
+    {
+        grafica.GUI();
+    }
+    public void registrazione()
+    {
+        String nome = JOptionPane.showInputDialog("Benvenuto nella fase di registrazione! \n" +
+                "Inserisci qui il tuo NOME");
+    }
 }
