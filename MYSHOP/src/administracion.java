332a5515219729f50033ea7253cc5f6c09b242d6
AlessioS98 <99815094+AlessioS98@users.noreply.github.com>
2022-02-28 15:46:26 +0100

    Add files via upload

--- MYSHOP/src/administracion.java
+++ MYSHOP/src/administracion.java
@@ -23,9 +23,29 @@ class Ascoltatore4 implements ActionListener
     }
 }
 
+class Ascoltatore6 implements ActionListener
+{
+    Admin rem = new Admin();
+    public void actionPerformed(ActionEvent event)
+    {
+        JButton b = (JButton)event.getSource();
+        rem.DeleteService();
+    }
+}
+
+class Ascoltatore7 implements ActionListener
+{
+    Admin mod = new Admin();
+    public void actionPerformed(ActionEvent event)
+    {
+        JButton b = (JButton)event.getSource();
+        mod.ModificationService();
+    }
+}
+
 public class administracion extends JFrame
 {
-    private JButton aggiungiProdottoButton;
+    private JButton aggiungiProfottoButton;
     private JButton modificaProdottoButton;
     private JButton eliminaProdottoButton;
     private JButton eliminaServizioButton;
@@ -34,13 +54,17 @@ public class administracion extends JFrame
     private JPanel administracionPanel;
     Ascoltatore5 listener5 = new Ascoltatore5();
     Ascoltatore4 listener4 = new Ascoltatore4();
+    Ascoltatore6 listener6 = new Ascoltatore6();
+    Ascoltatore7 listener7 = new Ascoltatore7();
 
     public administracion()
     {
         setContentPane(administracionPanel);
         setTitle("MODIFICA MYSHOP");
-        aggiungiProdottoButton.addActionListener(listener4);
+        aggiungiProfottoButton.addActionListener(listener4);
         aggiungiServizioButton.addActionListener(listener5);
+        eliminaServizioButton.addActionListener(listener6);
+        modificaServizioButton.addActionListener(listener7);
         setSize(500, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
