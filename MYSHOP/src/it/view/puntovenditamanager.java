3b329bd6779e3d49bcc6acab3fd90f42c374ac63
AlessioS98 <99815094+AlessioS98@users.noreply.github.com>
2022-03-06 00:45:15 +0100

    Add files via upload

--- MYSHOP/src/it/view/puntovenditamanager.java
+++ MYSHOP/src/it/view/puntovenditamanager.java
@@ -2,12 +2,14 @@ package it.view;
 
 import javax.swing.*;
 
-public class administracionpuntivendita {
+public class puntovenditamanager {
     private JTextField textField1;
     private JTextField textField2;
-    private JButton CONFERMAButton;
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField5;
-    private JButton CREAUNMANAGERButton;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JButton CONFERMAButton;
 }
