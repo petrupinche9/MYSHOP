2c10887d48bfde2adc595916f8e060d84409ea82
AlessioS98 <99815094+AlessioS98@users.noreply.github.com>
2022-03-07 00:35:36 +0100

    Add files via upload

--- MYSHOP/src/it/view/administracionpuntivendita.java
+++ MYSHOP/src/it/view/administracionpuntivendita.java
@@ -1,8 +1,19 @@
 package it.view;
+import it.model.Admin;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class administracionpuntivendita {
+class Ascoltatore implements ActionListener
+{
+    public void actionPerformed(ActionEvent event)
+    {
+        puntovenditamanager manager = new puntovenditamanager();
+    }
+}
+
+public class administracionpuntivendita extends JFrame {
     private JTextField textField1;
     private JTextField textField2;
     private JButton CONFERMAButton;
@@ -10,4 +21,15 @@ public class administracionpuntivendita {
     private JTextField textField4;
     private JTextField textField5;
     private JButton CREAUNMANAGERButton;
+    private JPanel managerPanel;
+    Ascoltatore listener = new Ascoltatore();
+
+    public administracionpuntivendita()
+    {
+        setContentPane(managerPanel);
+        setTitle("Creare Punti Vendita");
+        CREAUNMANAGERButton.addActionListener(listener);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
--- MYSHOP/src/it/view/puntovenditamanager.java
+++ MYSHOP/src/it/view/puntovenditamanager.java
@@ -2,7 +2,7 @@ package it.view;
 
 import javax.swing.*;
 
-public class puntovenditamanager {
+public class puntovenditamanager extends JFrame{
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
@@ -11,5 +11,14 @@ public class puntovenditamanager {
     private JTextField textField6;
     private JTextField textField7;
     private JTextField textField8;
+    private JPanel puntovenditamanagerPanel;
     private JButton CONFERMAButton;
+
+    public puntovenditamanager()
+    {
+        setContentPane(puntovenditamanagerPanel);
+        setTitle("Crea Manager");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
