fc20a44f2c02b012c55326f739f8c28c4cd9a643
AlessioS98 <99815094+AlessioS98@users.noreply.github.com>
2022-03-08 17:21:09 +0100

    Add files via upload

--- MYSHOP/src/it/view/administracionaggiungiprodotto.java
+++ MYSHOP/src/it/view/administracionaggiungiprodotto.java
@@ -1,17 +1,36 @@
 package it.view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class administracionaggiungiprodotto {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton CONFERMAButton1;
-    private JButton AGGIUNGISOTTOPRODOTTOButton;
-    private JButton CREAFORNITOREButton;
+public class administracionaggiungiprodotto extends JFrame {
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField5;
     private JTextField textField6;
     private JTextField textField7;
     private JTextField textField8;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JButton CONFERMAButton;
+    private JButton AGGIUNGISOTTOPRODOTTOButton;
+    private JPanel aggiungiprodottoPanel;
+
+    public administracionaggiungiprodotto()
+    {
+        setContentPane(aggiungiprodottoPanel);
+        setTitle("AGGIUNGI PRODOTTO");
+        setSize(500, 500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        CONFERMAButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
--- MYSHOP/src/it/view/administracionelimina.java
+++ MYSHOP/src/it/view/administracionelimina.java
@@ -1,9 +1,31 @@
 package it.view;
 
-import javax.swing.*;
+import it.DAO.AdminDAO;
 
-public class administracionelimina {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class administracionelimina extends JFrame{
     private JTextField textField1;
-    private JButton ELIMINASERVIZIOButton;
     private JButton ELIMINAARTICOLOButton;
+    private JPanel administracioneliminaPanel;
+
+    public administracionelimina()
+    {
+        setContentPane(administracioneliminaPanel);
+        setTitle("ELIMINA ARTICOLO");
+        setSize(500, 500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        ELIMINAARTICOLOButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String txtid = textField1.getText();
+                int id = Integer.parseInt(txtid);
+                AdminDAO delete = new AdminDAO();
+                delete.erase_article(id);
+            }
+        });
+    }
 }
