239b15435624154c30747641792a03eb12866388
AlessioS98 <99815094+AlessioS98@users.noreply.github.com>
2022-03-09 17:14:28 +0100

    Add files via upload

--- MYSHOP/src/it/view/administracionaggiungiservizio.java
+++ MYSHOP/src/it/view/administracionaggiungiservizio.java
@@ -1,12 +1,20 @@
 package it.view;
 
+import it.DAO.AdminDAO;
+import it.model.Fornitore;
+import it.model.service;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class administracionaggiungiservizio extends JFrame {
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
     private JTextField textField1;
-    private JTextField textField2;
     private JButton CONFERMAButton;
-    private JButton CREAFORNITOREButton;
     private JPanel aggiungiservicePanel;
 
     public administracionaggiungiservizio()
@@ -15,5 +23,20 @@ public class administracionaggiungiservizio extends JFrame {
         setTitle("AGGIUNGI SERVIZIO");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        CONFERMAButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String namefor = textField1.getText();
+                String namefornitore = textField3.getText();
+                String siteweb = textField4.getText();
+                String city = textField5.getText();
+                String nation = textField6.getText();
+                int id = (int)(Math.random()*100);
+                service newservice = new service(id, namefor);
+                Fornitore newfornitore = new Fornitore(id, namefornitore, siteweb, city, nation);
+                AdminDAO aggiungiservice = new AdminDAO();
+                aggiungiservice.newservice(newservice, newfornitore);
+            }
+        });
     }
 }
