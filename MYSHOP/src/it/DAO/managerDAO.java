f367ae10f382fb4be2b0be788a64a858f0400cc2
AlessioS98 <99815094+AlessioS98@users.noreply.github.com>
2022-04-08 11:32:53 +0200

    Add files via upload

--- MYSHOP/src/it/DAO/managerDAO.java
+++ MYSHOP/src/it/DAO/managerDAO.java
@@ -14,13 +14,14 @@ public class managerDAO implements ImanagerDAO{
     public manager findById(int id) {
         manager a = null;
 
-        ArrayList<String[]> res = DbConnection.getInstance().eseguiQuery("SELECT A.user_iduser, U.username, U.password, U.email FROM manager AS A INNER JOIN user as U  ON U.iduser = A.user_iduser WHERE A.user_iduser = "+id+";");
+        ArrayList<String[]> res = DbConnection.getInstance().eseguiQuery("SELECT A.user_iduser, U.username, U.passwd, U.email FROM manager AS A INNER JOIN user as U  ON U.iduser = A.user_iduser WHERE A.user_iduser = "+id+";");
 
         if(res.size()==1) {
             String[] riga = res.get(0);
             a = new manager();
             a.setId(Integer.parseInt(riga[0]));
             a.setUsername(riga[1]);
+            a.setPassword(riga[2]);
             a.setEmail(riga[3]);
         }
 
