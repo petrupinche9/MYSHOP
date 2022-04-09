2c3e28f1da18bf5c8a9be51ba963e835b6ebf749
AlessioS98 <99815094+AlessioS98@users.noreply.github.com>
2022-04-09 15:13:37 +0200

    Add files via upload

--- MYSHOP/src/it/view/Addusermanager.java
+++ MYSHOP/src/it/view/Addusermanager.java
@@ -1,10 +1,9 @@
 package it.view;
 
-import it.DAO.ShopDAO;
 import it.DAO.managerDAO;
 import it.DAO.userDAO;
-import it.model.Point_shop;
 import it.model.user;
+import it.util.Session;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -12,7 +11,6 @@ import java.awt.event.ActionListener;
 
 public class Addusermanager extends JFrame{
     private JTextField textField1;
-    private JTextField textField2;
     private JButton CONFERMAButton;
     private JPanel adduserPanel;
 
@@ -27,14 +25,10 @@ public class Addusermanager extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 String id = textField1.getText();
                 int iduser = Integer.parseInt(id);
-                String id2 = textField2.getText();
-                int idpoint = Integer.parseInt(id2);
                 userDAO userdao = new userDAO();
                 user adduser = userdao.findById(iduser);
-                ShopDAO shop = new ShopDAO();
-                Point_shop puntovendita = shop.findById(idpoint);
                 managerDAO manager = new managerDAO();
-                manager.add_user_to_shop(adduser, puntovendita);
+                manager.add_user_to_shop(adduser, manager.findById(Session.getInstance().getClienteLoggato().getId()));
                 JOptionPane.showMessageDialog(null, "Utente Aggiunto");
             }
         });
